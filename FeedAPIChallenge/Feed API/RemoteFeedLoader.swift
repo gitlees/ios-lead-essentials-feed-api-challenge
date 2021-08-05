bd2baeeab4f27a9b00b535dabb765efced4ee7ef
Stanislav Lee <glees769@gmail.com>
2021-08-05 20:43:10 +0600

    Complete with error `Invalid data` when client delivers non 200 HTTPURLResponse

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,7 +23,10 @@ public final class RemoteFeedLoader: FeedLoader {
 			switch result {
 			case .failure:
 				completion(.failure(Error.connectivity))
-			default: break
+			case let .success((_, response)):
+				if response.statusCode != 200 {
+					completion(.failure(Error.invalidData))
+				}
 			}
 		}
 	}
